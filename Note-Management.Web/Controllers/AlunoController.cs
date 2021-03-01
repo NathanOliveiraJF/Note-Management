a48ce9808d46c6e52b1854102437bb270dd6fe29
NathanOliveiraJF <nathanoliveira003jf@gmail.com>
2021-03-01 11:39:58 -0300

    Add project files.

---
+++ Note-Management.Web/Controllers/AlunoController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Note_Management.Web.Controllers
+{
+    public class AlunoController : Controller
+    {
+        public string Index()
+        {
+            return "This is my default action..";
+        }
+
+        //GET: /Aluno/Register
+        public string Register()
+        {
+            return "this is the Register action method"
+        }
+    }
+}
